8c5e71e7869c8eb188e7d7fe1ad31cf4feb818ae
Cole Nielson <cole.nielson.auto@gmail.com>
2025-04-11 00:18:06 -0600

    feat: Update site title and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 import { Providers } from "@/components/providers";
+import { ImageResponse } from 'next/og';
+import { Box } from 'lucide-react';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,8 +17,34 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export const size = { width: 32, height: 32 }
+
+export const icon = () => {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 24,
+          background: 'black',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '4px',
+        }}
+      >
+        <Box color="#ad4f11" size={24} />
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}
+
 export const metadata: Metadata = {
-  title: "LLM Chat UI",
+  title: "AI Garden",
   description: "Chat with different LLMs",
 };
 
